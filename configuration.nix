270a41fe89bbc94fee805a477bd083f19ed573f6
Bazinga9000 <bazonkidonk@gmail.com>
2024-09-27 21:30:12 -0400

    NTFS support

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,10 @@
   
   networking.hostName = "metatron"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
+  
+  # Enable NTFS Support
+  boot.supportedFilesystems = ["ntfs"];
+  
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
