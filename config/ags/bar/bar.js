4049a68d4fea1da17df9d968fa92e21808e66b53
Bazinga9000 <bazonkidonk@gmail.com>
2024-10-14 15:49:49 -0400

    removed ags import of notifcations

--- config/ags/bar/bar.js
+++ config/ags/bar/bar.js
@@ -1,6 +1,4 @@
 const hyprland = await Service.import("hyprland")
-const notifications = await Service.import("notifications")
-const mpris = await Service.import("mpris")
 const audio = await Service.import("audio")
 const battery = await Service.import("battery")
 const systemtray = await Service.import("systemtray")
