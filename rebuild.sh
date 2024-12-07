25a19e6d5eedf6f11e7bfa13d6f90e3e55e4a506
Bazinga9000 <bazonkidonk@gmail.com>
2024-12-06 21:42:06 -0500

    removed old shell scripts

--- rebuild.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-echo "Running nixos-rebuild"
-sudo nixos-rebuild switch --flake .#metatron
--- update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-echo "Running nix flake update"
-nix flake update
-./rebuild.sh
