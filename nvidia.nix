eea7e91c00d85637ee551f7c1a6833bbb400940d
Bazinga9000 <bazonkidonk@gmail.com>
2024-12-06 21:42:21 -0500

    made driver selection easier

--- nvidia.nix
+++ nvidia.nix
@@ -2,27 +2,11 @@
 
 { /* config, */ pkgs, ... }:
 let
-    # driver555 = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #   version = "555.58";
-    #   sha256_64bit = "sha256-bXvcXkg2kQZuCNKRZM5QoTaTjF4l2TtrsKUvyicj5ew=";
-    #   sha256_aarch64 = "sha256-7XswQwW1iFP4ji5mbRQ6PVEhD4SGWpjUJe1o8zoXYRE=";
-    #   openSha256 = "sha256-hEAmFISMuXm8tbsrB+WiUcEFuSGRNZ37aKWvf0WJ2/c=";
-    #   settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
-    #   persistencedSha256 = "sha256-lyYxDuGDTMdGxX3CaiWUh1IQuQlkI2hPEs5LI20vEVw=";
-    # };
-
-    # driver560 = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #   version = "560.35.03";
-    #   sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
-    #   sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
-    #   openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
-    #   settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
-    #   persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
-    # };
-
-    # driverLatest = pkgs.linuxPackages.nvidiaPackages.latest;
-    driverBeta = pkgs.linuxPackages.nvidiaPackages.beta;
-    driverPackage = driverBeta;
+    drivers = {
+      beta = pkgs.linuxPackages.nvidiaPackages.beta;
+      latest = pkgs.linuxPackages.nvidiaPackages.latest;
+    };
+    driverPackage = drivers.beta;
 in
 {
   # Enable graphics driver
