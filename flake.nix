9c8993cf406b760e0ffba2a1c5e4035b9b6b5510
Bazinga9000 <bazonkidonk@gmail.com>
2024-12-30 21:39:38 -0500

    removed dmraid tempfix

--- flake.nix
+++ flake.nix
@@ -48,17 +48,6 @@
             nixpkgs.overlays = [
               self.inputs.nix-alien.overlays.default # nix-alien
               self.inputs.purescript-overlay.overlays.default # purescript
-              # Temporary fix to get the build working until 368470 is merged
-              (final: (prev: {
-                dmraid = prev.dmraid.overrideAttrs (oA: {
-                  patches = oA.patches ++ [
-                    (prev.fetchpatch2 {
-                      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/f298cd74e67a841289fd0f10ef4ee85cfbbc4133/pkgs/os-specific/linux/dmraid/fix-dmevent_tool.patch";
-                      hash = "sha256-MmAzpdM3UNRdOk66CnBxVGgbJTzJK43E8EVBfuCFppc=";
-                    })
-                  ];
-                });
-              }))
             ];
 
             # Make the chosen overlays apply globally, not just in the flake build
