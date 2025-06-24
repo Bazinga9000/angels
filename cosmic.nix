41996e0a5b28f16ad6a0b51175e046d2d9177e30
Bazinga9000 <bazonkidonk@gmail.com>
2025-06-23 21:10:02 -0400

    re-enable gnome keyring

--- cosmic.nix
+++ cosmic.nix
@@ -1,4 +1,8 @@
 { pkgs, ... }: {
    services.displayManager.cosmic-greeter.enable = true;
    services.desktopManager.cosmic.enable = true;
+
+   # Enable GNOME Keyring Services
+   services.gnome.gnome-keyring.enable = true;
+   security.pam.services.gdm.enableGnomeKeyring = true;
 }
