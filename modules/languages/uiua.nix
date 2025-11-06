2b3c934c38d944a77759c9c0f9251792caf17a78
Bazinga9000 <bazonkidonk@gmail.com>
2025-11-06 17:27:37 -0500

    uiua: init

---
+++ modules/languages/uiua.nix
@@ -0,0 +1,20 @@
+{
+  flake.aspects.uiua = {
+    description = ''
+      Tools for Uiua
+    '';
+
+    nixos = {pkgs, ...}: {
+      environment.systemPackages = with pkgs; [
+        (uiua-unstable.override {
+          webcamSupport = true;
+          windowSupport = true;
+        })
+      ];
+
+      fonts.packages = with pkgs; [
+        uiua386
+      ];
+    };
+  };
+}
