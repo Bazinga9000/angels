3f22517fe1cd51dd9c0bca52353f38dd407649e1
Bazinga9000 <bazonkidonk@gmail.com>
2025-11-21 23:40:50 -0500

    flake: update

--- modules/core-inputs/dendritic.nix
+++ modules/core-inputs/dendritic.nix
@@ -1,7 +1,11 @@
 # Enable various modules for the Dendritic Pattern (e.g den, flake-aspects)
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
+  flake-file.inputs.flake-file.url = lib.mkDefault "github:vic/flake-file";
+  flake-file.inputs.den.url = lib.mkDefault "github:vic/den";
   imports = [
     inputs.flake-file.flakeModules.dendritic
+    inputs.den.flakeModules.dendritic
+    inputs.flake-aspects.flakeModule
   ];
 }
