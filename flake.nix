173ef30abd22ebca5d08abde2eaad8ca38ec9347
Bazinga9000 <bazonkidonk@gmail.com>
2026-03-12 02:10:01 -0400

    flake: update

--- flake.nix
+++ flake.nix
@@ -1,82 +1,80 @@
 # DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
-
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
     baz9k-pkgs = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:Bazinga9000/baz9k-pkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     bizhawk = {
-      flake = false;
       url = "github:TASEmulators/BizHawk";
+      flake = false;
     };
     den.url = "github:vic/den";
     flake-aspects.url = "github:vic/flake-aspects";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-file.url = "github:vic/flake-file";
     flake-parts = {
-      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
       url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
     };
     home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     hytale-launcher = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:TNAZEP/HytaleLauncherFlake";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree.url = "github:vic/import-tree";
     niri-flake = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-alien.url = "github:thiagokokada/nix-alien";
     nix-index-database = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-vscode-extensions = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-lib.follows = "nixpkgs";
     nixpkgs-patch-hytale = {
-      flake = false;
       url = "https://github.com/NixOS/nixpkgs/pull/479368.diff";
+      flake = false;
     };
     nixpkgs-patcher.url = "github:gepbird/nixpkgs-patcher";
     noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         noctalia-qs.follows = "noctalia-qs";
       };
-      url = "github:noctalia-dev/noctalia-shell";
     };
     noctalia-qs = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:noctalia-dev/noctalia-qs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     purescript-overlay = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:thomashoneyman/purescript-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     uiua = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:uiua-lang/uiua";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
 }
