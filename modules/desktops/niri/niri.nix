ec108421d245beafb83db740791486f9113e3844
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-03-28 20:31:20 -0400

    niri: switch to gnome polkit agent

--- modules/desktops/niri/niri.nix
+++ modules/desktops/niri/niri.nix
@@ -62,6 +62,23 @@
           nautilus
           nautilus-python
         ];
+
+        # Use GNOME polkit instead of kde
+        systemd.user.services.niri-flake-polkit.enable = false;
+        systemd.user.services.polkit-gnome-authentication-agent-1 = {
+          description = "polkit-gnome-authentication-agent-1";
+          wantedBy = [ "graphical-session.target" ];
+          wants = [ "graphical-session.target" ];
+          after = [ "graphical-session.target" ];
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+            Restart = "on-failure";
+            RestartSec = 1;
+            TimeoutStopSec = 10;
+          };
+        };
+
       };
 
     homeManager =
