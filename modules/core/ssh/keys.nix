0ad4bebd6c3ae526192d0b447b2aa291f8920809
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-04-14 21:26:41 -0400

    ssh: add baraqiel keys

--- modules/core/ssh/keys.nix
+++ modules/core/ssh/keys.nix
@@ -9,6 +9,12 @@ let
       root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3vqUDsF4+l55ZNNAuDsEPTS/lr0jhD2QkYwdgl+78Y root@jophiel";
       bazinga = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKwaXZxgCMvBaEs0Fxr/cM5eauwXiDMjq2QeL0j5qhv1 bazinga@jophiel";
     };
+
+    baraqiel = {
+      # The phone isn't rooted, so we're lying about the usernames. but whatever.
+      root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6Vu0B630Sqc9j6WWftKRKZPSN17vlst+QyQaoMgqo7 u0_a339@localhost";
+      bazinga = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPekt+iFjr1ZYsrmaMcDkDwgywqb+bFOIULR5VhTeBB u0_a339@localhost";
+    };
   };
 in
 {
