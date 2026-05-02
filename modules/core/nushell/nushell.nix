dc7246e3526cf4ec7dd0114b98b668925637eb9f
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-05-02 12:37:54 -0400

    nushell: use nix-your-shell to put nu in nix shells

--- modules/core/nushell/nushell.nix
+++ modules/core/nushell/nushell.nix
@@ -12,27 +12,36 @@
         users.defaultUserShell = pkgs.nushell;
       };
 
-    homeManager = {
-      # Enable and configure Nushell via Home Manager
-      programs.nushell = {
-        enable = true;
-        configFile.source = ./config.nu;
-        shellAliases = {
-          angels = "just --justfile=$'/home/(whoami)/angels/justfile'";
+    homeManager =
+      { config, pkgs, ... }:
+      {
+        # Enable and configure Nushell via Home Manager
+        programs.nushell = {
+          enable = true;
+          configFile.source = ./config.nu;
+          shellAliases = {
+            angels = "just --justfile=$'/home/(whoami)/angels/justfile'";
+          };
+
+          extraConfig = ''
+            source nix-your-shell.nu
+          '';
         };
-      };
 
-      # Enable autocompletion in the shell via Carapace
-      programs.carapace = {
-        enable = true;
-        enableNushellIntegration = true;
-      };
+        # Enable autocompletion in the shell via Carapace
+        programs.carapace = {
+          enable = true;
+          enableNushellIntegration = true;
+        };
 
-      # Enable Starship for prompt
-      programs.starship = {
-        enable = true;
-        enableNushellIntegration = true;
+        # Enable Starship for prompt
+        programs.starship = {
+          enable = true;
+          enableNushellIntegration = true;
+        };
+
+        home.file."${config.xdg.configHome}/nushell/nix-your-shell.nu".source =
+          pkgs.nix-your-shell.generate-config "nu";
       };
-    };
   };
 }
