2228ce48ac344d0c52b547a69e8911a31c51aa8b
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-05-10 19:52:02 +0200

    locale: enable automatic timezone

--- modules/core/locale.nix
+++ modules/core/locale.nix
@@ -4,24 +4,27 @@
       Configure Locale and Time information
     '';
 
-    nixos = {
-      # Set your time zone.
-      time.timeZone = "America/New_York";
+    nixos =
+      { lib, ... }:
+      {
+        # Set your time zone.
+        services.automatic-timezoned.enable = true;
+        time.timeZone = lib.mkDefault "America/New_York";
 
-      # Select internationalisation properties.
-      i18n.defaultLocale = "en_US.UTF-8";
+        # Select internationalisation properties.
+        i18n.defaultLocale = "en_US.UTF-8";
 
-      i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_US.UTF-8";
-        LC_IDENTIFICATION = "en_US.UTF-8";
-        LC_MEASUREMENT = "en_US.UTF-8";
-        LC_MONETARY = "en_US.UTF-8";
-        LC_NAME = "en_US.UTF-8";
-        LC_NUMERIC = "en_US.UTF-8";
-        LC_PAPER = "en_US.UTF-8";
-        LC_TELEPHONE = "en_US.UTF-8";
-        LC_TIME = "en_US.UTF-8";
+        i18n.extraLocaleSettings = {
+          LC_ADDRESS = "en_US.UTF-8";
+          LC_IDENTIFICATION = "en_US.UTF-8";
+          LC_MEASUREMENT = "en_US.UTF-8";
+          LC_MONETARY = "en_US.UTF-8";
+          LC_NAME = "en_US.UTF-8";
+          LC_NUMERIC = "en_US.UTF-8";
+          LC_PAPER = "en_US.UTF-8";
+          LC_TELEPHONE = "en_US.UTF-8";
+          LC_TIME = "en_US.UTF-8";
+        };
       };
-    };
   };
 }
